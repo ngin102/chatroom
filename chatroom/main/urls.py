513fead391a362669f16307714513478ff6ebcd1
Nicholas Gin <ngin102@uottawa.ca>
2023-07-09 20:26:08 -0400

    Update mainpage to sign up page

--- chatroom/main/urls.py
+++ chatroom/main/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage, name = 'mainpage'),
+    path('', views.signup, name = 'signup'),
     path('chatrooms/', include('room.urls')),
     path('signup/', views.signup, name = 'signup'),
     path('signin/', auth_views.LoginView.as_view(template_name = 'signin.html'), name = 'signin'),
